d7bd34722aef194316daced233864bbb291fdc2f
Mateo Vallejo <mateovallejo@MacBook-Pro-de-Mateo-2.local>
2022-09-05 16:04:49 -0300

    Ver Mas y estilos

--- src/components/Peliculas/PeliculaPopuCard.js
+++ src/components/Peliculas/PeliculaPopuCard.js
@@ -1,27 +1,35 @@
-import React, { Component } from 'react';
-import './PeliculaPopuCard.css'
+import React, { Component } from "react";
+import "./PeliculaPopuCard.css";
 
 class PeliculaPopuCard extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+        verMas: false
+    };
+  }
 
-    render() {
-        return (
-            <article className='character-card'>
-                <h2>{this.props.datosPelicula.title}</h2>
-                <p>{this.props.datosPelicula.overview}</p>
-                <img src={'https://image.tmdb.org/t/p/original/' + this.props.datosPelicula.backdrop_path
-                } alt="" />
+  masMenosInfo() {
+    this.setState({
+      verMas: !this.state.verMas
+    });
+  }
 
-
-
-            </article>
-
-        )
-    }
-
+  render() {
+    return (
+      <article className="pelicula-card">
+        <img src={"https://image.tmdb.org/t/p/original/" + this.props.datosPelicula.backdrop_path}alt=""/>
+        <h2>{this.props.datosPelicula.title}</h2>
+        <p>{this.props.datosPelicula.release_date}</p>
+        {
+        this.state.verMas 
+        ? <p>{this.props.datosPelicula.overview}</p> 
+        : <p>{this.props.datosPelicula.overview.slice(0, 100)} [...]</p>
+        }
+        <a onClick={() => this.masMenosInfo()} href='#'> {this.state.verMas ? "Ver menos" : "Ver mas"} </a>
+      </article>
+    );
+  }
 }
 
-export default PeliculaPopuCard
+export default PeliculaPopuCard;
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,35 +1,35 @@
 import React, {Component} from 'react';
 import PeliculaPopuCard from '../../components/Peliculas/PeliculaPopuCard';
+import './Home.css'
 
 class Home extends Component{
     constructor(){
         super()
-        this.state={
+        this.state = {
            peliculas:[], //aparecer personajes
-            nextUrl:''
+           nextUrl:''
         }
     }
     componentDidMount(){
         //BUscamos datos
         fetch('https://api.themoviedb.org/3/movie/popular?api_key=0e7a6bf53a9c840b66557a6d28ea5004&language=en-US&page=1 ')
-           
         .then( res => res.json())
         .then( data => this.setState({
                 peliculas: data.results
-            },()=>console.log(this.state.peliculas)
+            },() => console.log(this.state.peliculas)
             ))
             .catch()
     }
     render(){
         return(
-            <React.Fragment>
-              {
-                this.state.peliculas.map((pelicula, idx) => <PeliculaPopuCard key={pelicula.id} datosPelicula={pelicula} />)
-              }
+            <React.Fragment >
+                <section className="contenedor-card">
+                    {
+                        this.state.peliculas.map((pelicula, idx) => <PeliculaPopuCard key={pelicula + idx} datosPelicula={pelicula} />)
+                    }  
+                </section>
             </React.Fragment>
         )
     }
-
-
 }
-export default Home
+export default Home;
