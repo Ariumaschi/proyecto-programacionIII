21b82c3472a82173d43d215596992d55dd6b8ab9
Clarita Ottonello <ClaritaOttonello@MacBookdeClara.fibertel.com.ar>
2022-09-07 01:04:47 -0300

    avanzo con home

---
+++ src/components/Billboard/Billboard.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./Billboard.css";
+
+class Billboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        verMas: false
+    };
+  }
+
+  masMenosInfo() {
+    this.setState({
+      verMas: !this.state.verMas
+    });
+  }
+
+  render() {
+    return (
+      <article className="pelicula-card">
+        <img src={"https://image.tmdb.org/t/p/original/" + this.props.datosPelicula.poster_path}alt=""/>
+        <h2>{this.props.datosPelicula.title} ({this.props.datosPelicula.release_date.slice(0,4)})</h2>
+        {
+        this.state.verMas 
+        ? <p>{this.props.datosPelicula.overview}</p> 
+        : <p>{this.props.datosPelicula.overview.slice(0, 100)} [...]</p>
+        }
+        <a onClick={() => this.masMenosInfo()} href=''> {this.state.verMas ? "Ver menos" : "Ver mas"} </a>
+      </article>
+    );
+  }
+}
+
+export default Billboard;
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Billboard from '../../components/Billboard/Billboard';
 import PeliculaPopuCard from '../../components/PeliculaPopuCard/PeliculaPopuCard';
 import './Home.css'
 
@@ -6,28 +7,49 @@ class Home extends Component{
     constructor(){
         super()
         this.state = {
-           peliculas:[], //aparecer personajes
-           nextUrl:''
+            key:'0e7a6bf53a9c840b66557a6d28ea5004',
+            popularMovies:[], //aparecer personajes
+            cartelMovies: [],
+            nextUrl:'',
+
+        
         }
     }
     componentDidMount(){
         //Buscamos datos
-        fetch('https://api.themoviedb.org/3/movie/popular?api_key=0e7a6bf53a9c840b66557a6d28ea5004&language=en-US&page=1')
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=' + this.state.key)
         .then( res => res.json())
         .then( data => this.setState({
-                peliculas: data.results
-            },() => console.log(this.state.peliculas)
+            popularMovies: data.results
+            },() => console.log(this.state.popularMovies)
             ))
-            .catch()
+        .catch()
+
+        fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=' + this.state.key)
+        .then( res => res.json())
+        .then( data => this.setState({
+            cartelMovies: data.results
+            }))
+        .catch()
+
     }
     render(){
         return(
-            <React.Fragment >
+            <React.Fragment>
+                 
+                <h1 className="h1"> Más Populares </h1>
                 <section className="contenedor-card">
-                    {
-                        this.state.peliculas.map((pelicula, idx) => <PeliculaPopuCard key={pelicula + idx} datosPelicula={pelicula} />)
-                    }  
+                {
+                    this.state.popularMovies.map((pelicula, idx) => <PeliculaPopuCard key={pelicula + idx} datosPelicula={pelicula} />)
+                }  
                 </section>
+                <h1 className="h1">En cartelera</h1>
+                <section className="contenedor-card">
+                {
+                    this.state.cartelMovies.map((cartelMovie, idx) => <Billboard key={cartelMovie + idx} datosPelicula={cartelMovie} />)
+                }  
+                </section>
+
             </React.Fragment>
         )
     }
