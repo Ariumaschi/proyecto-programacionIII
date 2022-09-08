40659f26c1a9f4a317310c69ab3d693d30008506
Clarita Ottonello <ClaritaOttonello@MacBookdeClara.fibertel.com.ar>
2022-09-08 16:12:40 -0300

    avanzamos con search

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Billboard from '../../components/Billboard/Billboard';
 import PeliculaPopuCard from '../../components/PeliculaPopuCard/PeliculaPopuCard';
 import './Home.css'
+import Form from'../../components/Form/Form'
+import {Link} from 'react-router-dom'
 
 class Home extends Component{
     constructor(){
@@ -12,6 +14,7 @@ class Home extends Component{
             cartelMovies: [],
             nextUrl:'',
             valor: '',
+            peliculasBuscadas: []
 
         
         }
@@ -34,43 +37,34 @@ class Home extends Component{
         .catch()
 
     }
-    evitarSubmit(e) {
-        e.preventDefault();
-    }
-    controlarCambios(e) {
+    guardarPeliculasBuscadas(buscado){
         this.setState({
-          valor: e.target.value,
-      }, () => {
-          fetch('https://api.themoviedb.org/3/search/movie?api_key=' + this.state.key + '&query=' + this.state.valor)
-              .then(data => data.json())
-              .then(info => {
-                  this.setState({
-                      resultados: info.results
-                  })
-              })
-          
-      })         
-  }
+            peliculasBuscadas: buscado//Las películas que te pasa elform
+        })
+    }
 
     render(){
         return(
             <React.Fragment>
-                 <form onSubmit={(e) => this.evitarSubmit(e)}>
-                    <input className="form" type="text" onChange={(e) => this.controlarCambios(e)} />
-                    <button className="form" type="submit">Buscar</button>
-                </form>
+                 <Form />
                  
                 <h1 className="h1"> Más Populares </h1>
                 <section className="contenedor-card">
                 {
                     this.state.popularMovies.map((pelicula, idx) => <PeliculaPopuCard key={pelicula + idx} datosPelicula={pelicula} />)
                 }  
+                    <div className="cont-vermas">
+                    <Link className="Link" to='/todas'> Ver Todas </Link>  
+                </div>
                 </section>
                 <h1 className="h1">En cartelera</h1>
                 <section className="contenedor-card">
                 {
                     this.state.cartelMovies.map((cartelMovie, idx) => <Billboard key={cartelMovie + idx} datosPelicula={cartelMovie} />)
                 }  
+                    <div className="cont-vermas">
+                    <Link className="Link" to='/todas'> Ver Todas </Link>  
+                </div>
                 </section>
 
             </React.Fragment>
